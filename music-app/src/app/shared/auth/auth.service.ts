2140e69221738309b179b69ff52534ff76d5500a
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-11-14 20:17:13 -0600

    Update auth.service.ts

--- music-app/src/app/shared/auth/auth.service.ts
+++ music-app/src/app/shared/auth/auth.service.ts
@@ -126,6 +126,11 @@ export class AuthService{
   logout(){
     this.user.next(null);
     this.router.navigate(['/home']);
+    localStorage.removeItem('userData');
+    if(this.tokenExpirationTimer){
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer = null;
   }
 
   autoLogout(expirationDuration: number){
