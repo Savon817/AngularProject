8ef8f4ab0397b979e8dd8f4f7c582307ada7fb80
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-11-17 18:32:31 -0600

    Added Dummy Data

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'music-app';
+  imgPath = 'https://i.discogs.com/kouW-_83Tavb9vv2Yt7Axd6L__8866h7n7tRLjWjQpc/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4MTI5/NjcwLTE2MTc0MjIx/NDgtMzQzNC5qcGVn.jpeg';
 }
