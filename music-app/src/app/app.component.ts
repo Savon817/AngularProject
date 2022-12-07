fdc75c5f0be4e8c4e24c5d25ac0508de209a5488
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-12-06 20:38:06 -0600

    Update: Fixed autologin

--- music-app/src/app/app.component.ts
+++ music-app/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService){}
+
   title = 'music-app';
   imgPath = 'https://i.discogs.com/kouW-_83Tavb9vv2Yt7Axd6L__8866h7n7tRLjWjQpc/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4MTI5/NjcwLTE2MTc0MjIx/NDgtMzQzNC5qcGVn.jpeg';
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
 }
--- music-app/src/app/shared/auth/auth.service.ts
+++ music-app/src/app/shared/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { BehaviorSubject, catchError, tap, throwError } from "rxjs";
+
 import { User } from "./user.model";
 
 //Firebase Crendentials
